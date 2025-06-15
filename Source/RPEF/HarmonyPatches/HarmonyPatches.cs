e1ce7a69763ed5da7b59ed6181bcc5008b2aa987
gguake <gguak2@gmail.com>
2025-06-15 21:16:56 +0900

    비행관련 기능 추가

--- Source/RPEF/HarmonyPatches/HarmonyPatches.cs
+++ Source/RPEF/HarmonyPatches/HarmonyPatches.cs
@@ -103,6 +103,14 @@ namespace RPEF
                     original: AccessTools.Method(typeof(DynamicPawnRenderNodeSetup_Apparel), "ProcessApparel"),
                     postfix: new HarmonyMethod(typeof(HarmonyPatches), nameof(DynamicPawnRenderNodeSetup_Apparel_ProcessApparel)));
 
+                harmony.Patch(
+                    original: AccessTools.Method(typeof(Pawn_FlightTracker), nameof(Pawn_FlightTracker.GetBestFlyAnimation)),
+                    transpiler: new HarmonyMethod(typeof(HarmonyPatches), nameof(Pawn_FlightTracker_GetBestFlyAnimation_Transpiler)));
+
+                harmony.Patch(
+                    original: AccessTools.PropertyGetter(typeof(Pawn_FlightTracker), nameof(Pawn_FlightTracker.CanFlyNow)),
+                    postfix: new HarmonyMethod(typeof(HarmonyPatches), nameof(Pawn_FlightTracker_CanFlyNow_getter_Postfix)));
+
                 RestrictionPatches.Patch(harmony);
             }
             catch (Exception e)
@@ -571,6 +579,102 @@ namespace RPEF
                 }
 
                 __result.parent = parentNode;
+            }
+        }
+
+        private static AnimationDef Pawn_FlightTracker_GetBestFlyAnimation_Injection(Pawn pawn)
+        {
+            var modExtension = pawn.def.GetModExtension<HumanlikeFlyExtension>();
+            if (modExtension != null)
+            {
+                var data = modExtension.animationData.FirstOrDefault(v => v.lifeStage == pawn.ageTracker.CurLifeStage);
+                if (data != null)
+                {
+                    var rot = pawn.Rotation;
+                    if (rot == Rot4.South)
+                    {
+                        return data.animationSouth;
+                    }
+                    else if (rot == Rot4.North)
+                    {
+                        return data.animationNorth;
+                    }
+                    else if (rot == Rot4.East)
+                    {
+                        return data.animationEast;
+                    }
+                    else
+                    {
+                        return data.animationWest;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private static IEnumerable<CodeInstruction> Pawn_FlightTracker_GetBestFlyAnimation_Transpiler(IEnumerable<CodeInstruction> codeInstructions)
+        {
+            var instructions = codeInstructions.ToList();
+
+            var index = instructions.FindIndex(
+                v =>
+                v.opcode == OpCodes.Callvirt &&
+                v.OperandIs(AccessTools.PropertyGetter(typeof(RaceProperties), nameof(RaceProperties.Humanlike))));
+
+            if (index >= 0)
+            {
+                instructions.RemoveAt(index + 2);
+                instructions.RemoveAt(index + 2);
+                instructions.InsertRange(index + 2, new CodeInstruction[]
+                {
+                    new CodeInstruction(OpCodes.Ldarg_0),
+                    new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(HarmonyPatches), nameof(Pawn_FlightTracker_GetBestFlyAnimation_Injection))),
+                    new CodeInstruction(OpCodes.Ret),
+                });
+
+            }
+            else
+            {
+                Log.Error($"[RPEF] Failed to find injection point for Pawn_FlightTracker_GetBestFlyAnimation_Transpiler");
+            }
+
+            return instructions;
+        }
+
+        private static void Pawn_FlightTracker_CanFlyNow_getter_Postfix(Pawn ___pawn, ref bool __result)
+        {
+            if (__result && !___pawn.flight.Flying)
+            {
+                var modExtension = ___pawn.def.GetModExtension<HumanlikeFlyExtension>();
+                if (modExtension != null)
+                {
+                    var data = modExtension.animationData.FirstOrDefault(v => v.lifeStage == ___pawn.ageTracker.CurLifeStage);
+                    if (data != null)
+                    {
+                        if (modExtension.requireBodyParts != null)
+                        {
+                            for (int i = 0; i < ___pawn.health.hediffSet.hediffs.Count; ++i)
+                            {
+                                var hediff = ___pawn.health.hediffSet.hediffs[i];
+                                if (hediff is Hediff_MissingPart hediffMissingPart && hediffMissingPart.Part != null)
+                                {
+                                    for (int j = 0; j < modExtension.requireBodyParts.Count; ++j)
+                                    {
+                                        Log.Message($"{modExtension.requireBodyParts[j]} {hediffMissingPart.Part.untranslatedCustomLabel}");
+                                        if (modExtension.requireBodyParts[j] == hediffMissingPart.Part.untranslatedCustomLabel)
+                                        {
+                                            __result = false;
+                                            return;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+
             }
         }
     }
---
+++ Source/RPEF/ModExtensions/HumanlikeFlyExtension.cs
@@ -0,0 +1,28 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using System.Xml;
+using Verse;
+
+namespace RPEF
+{
+    public class HumanlikeFlyAnimationData
+    {
+        public LifeStageDef lifeStage;
+        public AnimationDef animationSouth;
+        public AnimationDef animationNorth;
+        public AnimationDef animationEast;
+        public AnimationDef animationWest;
+
+        public void LoadDataFromXmlCustom(XmlNode xmlRoot)
+        {
+            XmlHelper.ParseElements(this, xmlRoot, "lifeStage");
+        }
+    }
+
+    public class HumanlikeFlyExtension : DefModExtension
+    {
+        public List<HumanlikeFlyAnimationData> animationData;
+
+        public List<string> requireBodyParts;
+    }
+}

91b7467c4662660741e3e355ddd8cc10e31ea625
gguake <gguak2@gmail.com>
2026-03-18 00:47:05 +0900

    슬립 페이스 기능 추가

---
+++ Source/RPEF/RenderNode/PawnRenderNodeWorker_ClosedEye.cs
@@ -0,0 +1,17 @@
+﻿using RimWorld;
+using Verse;
+
+namespace RPEF
+{
+    public class PawnRenderNodeWorker_ClosedEye : PawnRenderNodeWorker
+    {
+        public override bool CanDrawNow(PawnRenderNode node, PawnDrawParms parms)
+        {
+            if (!base.CanDrawNow(node, parms)) { return false; }
+
+            if (!parms.pawn.DeadOrDowned && parms.pawn.Awake()) { return false; }
+
+            return true;
+        }
+    }
+}
---
+++ Source/RPEF/RenderNode/PawnRenderNode_HeadMesh.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace RPEF
+{
+    public class PawnRenderNode_HeadMesh : PawnRenderNode_Head
+    {
+        public PawnRenderNode_HeadMesh(Pawn pawn, PawnRenderNodeProperties props, PawnRenderTree tree) 
+            : base(pawn, props, tree)
+        {
+        }
+
+        public override Graphic GraphicFor(Pawn pawn)
+        {
+            if (!pawn.health.hediffSet.HasHead)
+            {
+                return null;
+            }
+
+            var path = TexPathFor(pawn);
+            if (path.NullOrEmpty())
+            {
+                return null;
+            }
+
+            var shader = ShaderFor(pawn);
+            if (shader == null)
+            {
+                return null;
+            }
+
+            return GraphicDatabase.Get<Graphic_Multi>(path, shader, Vector2.one, ColorFor(pawn));
+        }
+    }
+}

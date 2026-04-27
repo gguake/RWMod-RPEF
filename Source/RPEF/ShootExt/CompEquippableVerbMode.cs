aa2225c5aa6d3119eca349eda343511aac0b2338
gguake <gguak2@gmail.com>
2026-04-27 19:40:07 +0900

    shootwithmode 추가

---
+++ Source/RPEF/ShootExt/CompEquippableVerbMode.cs
@@ -0,0 +1,190 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Verse;
+using Verse.AI;
+
+namespace RPEF
+{
+    public class VerbMode
+    {
+        public string label;
+        public string description;
+
+        public string changeGizmoLabel;
+        public string changeGizmoDescription;
+
+        public string customLabel;
+
+        [NoTranslate]
+        public string uiIconPath;
+
+        [Unsaved(false)]
+        private Texture2D uiIconCached;
+
+        public Texture2D UIIcon
+        {
+            get
+            {
+                if (uiIconCached == null && !uiIconPath.NullOrEmpty())
+                {
+                    uiIconCached = ContentFinder<Texture2D>.Get(uiIconPath);
+                }
+                return uiIconCached;
+            }
+        }
+
+        public int swapTicks = 300;
+
+        public int? shotsPerBurstOverride;
+        public float? effectiveRangeOverride;
+        public float? warmupTimeOverride;
+        public ThingDef projectileDefOverride;
+
+        public int projectilesPerShot = 1;
+
+        public List<StatModifier> statOffsets;
+        public List<StatModifier> statFactors;
+
+        private Dictionary<StatDef, StatModifier> _statOffsetDict;
+        public float GetStatOffset(StatDef stat)
+        {
+            if (_statOffsetDict == null)
+            {
+                _statOffsetDict = statOffsets?.ToDictionary(e => e.stat) ?? new Dictionary<StatDef, StatModifier>();
+            }
+
+            return _statOffsetDict.TryGetValue(stat, out var modifier) ? modifier.value : 0f;
+        }
+
+
+        private Dictionary<StatDef, StatModifier> _statFactorDict;
+        public float GetStatFactor(StatDef stat)
+        {
+            if (_statFactorDict == null)
+            {
+                _statFactorDict = statFactors?.ToDictionary(e => e.stat) ?? new Dictionary<StatDef, StatModifier>();
+            }
+
+            return _statFactorDict.TryGetValue(stat, out var modifier) ? modifier.value : 1f;
+        }
+    }
+
+    public class CompProperties_EquippableVerbMode : CompProperties
+    {
+        public List<VerbMode> modes;
+        public bool transformLabelOnlyEquipped = true;
+
+        public CompProperties_EquippableVerbMode()
+        {
+            compClass = typeof(CompEquippableVerbMode);
+        }
+    }
+
+    public class CompEquippableVerbMode : CompEquippable
+    {
+        public CompProperties_EquippableVerbMode Props => (CompProperties_EquippableVerbMode)props;
+
+        public VerbMode CurrentVerbMode => Props.modes[_verbModeIndex];
+
+        private int _verbModeIndex;
+
+        public override void PostExposeData()
+        {
+            base.PostExposeData();
+
+            Scribe_Values.Look(ref _verbModeIndex, "verbModeIndex");
+
+            if (Scribe.mode == LoadSaveMode.PostLoadInit)
+            {
+                if (_verbModeIndex < 0 || _verbModeIndex >= Props.modes.Count)
+                {
+                    _verbModeIndex = 0;
+                }
+            }
+        }
+
+        public override IEnumerable<Gizmo> CompGetEquippedGizmosExtra()
+        {
+            if (Holder?.Drafted != true || Props.modes.NullOrEmpty() || Props.modes.Count < 2)
+            {
+                yield break;
+            }
+
+            var current = CurrentVerbMode;
+            yield return new Command_Action
+            {
+                defaultLabel = current.label,
+                defaultDesc = current.description,
+                icon = current.UIIcon,
+                action = () =>
+                {
+                    if (Props.modes.Count == 2)
+                    {
+                        ChangeMode((_verbModeIndex + 1) % 2);
+                    }
+                    else
+                    {
+                        var options = new List<FloatMenuOption>();
+                        for (int i = 0; i < Props.modes.Count; i++)
+                        {
+                            if (i == _verbModeIndex)
+                            {
+                                continue;
+                            }
+
+                            int targetIndex = i;
+                            var mode = Props.modes[i];
+                            options.Add(new FloatMenuOption(
+                                label: mode.changeGizmoLabel ?? mode.label,
+                                action: () => ChangeMode(targetIndex),
+                                iconTex: mode.UIIcon,
+                                iconColor: Color.white)); ;
+                        }
+
+                        if (options.Count > 0)
+                        {
+                            Find.WindowStack.Add(new FloatMenu(options));
+                        }
+                    }
+                },
+            };
+        }
+
+        public override string TransformLabel(string label)
+        {
+            if (CurrentVerbMode.customLabel == null || 
+                (Props.transformLabelOnlyEquipped && Holder == null))
+            {
+                return base.TransformLabel(label);
+            }
+
+            return string.Format(CurrentVerbMode.customLabel, base.TransformLabel(label));
+        }
+
+        public override float GetStatOffset(StatDef stat)
+            => CurrentVerbMode.GetStatOffset(stat);
+
+        public override float GetStatFactor(StatDef stat)
+            => CurrentVerbMode.GetStatFactor(stat);
+
+        private void ChangeMode(int modeIndex)
+        {
+            _verbModeIndex = modeIndex;
+
+            var ticks = Props.modes[modeIndex].swapTicks;
+            if (ticks <= 0)
+            {
+                return;
+            }
+
+            var pawn = Holder;
+            if (pawn?.jobs != null)
+            {
+                var waitJob = JobMaker.MakeJob(JobDefOf.Wait, ticks, true);
+                pawn.jobs.StartJob(waitJob, JobCondition.InterruptForced);
+            }
+        }
+    }
+}
---
+++ Source/RPEF/ShootExt/Verb_ShootWithMode.cs
@@ -0,0 +1,127 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using UnityEngine;
+using Verse;
+
+namespace RPEF
+{
+    public class Verb_ShootWithMode : Verb_Shoot
+    {
+        public CompEquippableVerbMode EquipmentCompVerbModeSource => DirectOwner as CompEquippableVerbMode;
+
+        protected override int ShotsPerBurst => 
+            EquipmentCompVerbModeSource?.CurrentVerbMode.shotsPerBurstOverride ??
+            base.ShotsPerBurst;
+
+        public override float EffectiveRange
+        {
+            get
+            {
+                var overrideRange = EquipmentCompVerbModeSource?.CurrentVerbMode.effectiveRangeOverride;
+                if (overrideRange.HasValue)
+                {
+                    return overrideRange.Value * (EquipmentSource?.GetStatValue(StatDefOf.RangedWeapon_RangeMultiplier) ?? 1f);
+                }
+
+                return base.EffectiveRange;
+            }
+        }
+
+        public override float WarmupTime
+        {
+            get
+            {
+                var overrideWarmupTime = EquipmentCompVerbModeSource?.CurrentVerbMode.warmupTimeOverride;
+                if (overrideWarmupTime.HasValue)
+                {
+                    return overrideWarmupTime.Value * (EquipmentSource?.GetStatValue(StatDefOf.RangedWeapon_WarmupMultiplier) ?? 1f);
+                }
+
+                return base.WarmupTime;
+            }
+        }
+
+        public override ThingDef Projectile =>
+            EquipmentCompVerbModeSource?.CurrentVerbMode.projectileDefOverride ?? 
+            base.Projectile;
+
+        protected override bool TryCastShot() => Verb_ShootWithMode_Patch.TryCastShot_LaunchProjectile(this);
+    }
+
+    [HarmonyPatch]
+    internal static class Verb_ShootWithMode_Patch
+    {
+        [HarmonyReversePatch]
+        [HarmonyPatch(typeof(Verb_LaunchProjectile), "TryCastShot")]
+        public static bool TryCastShot_LaunchProjectile(Verb_LaunchProjectile instance)
+        {
+            IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var methodInfoLaunch = AccessTools.Method(
+                    typeof(Projectile),
+                    nameof(Projectile.Launch),
+                    new Type[]
+                    {
+                        typeof(Thing),
+                        typeof(Vector3),
+                        typeof(LocalTargetInfo),
+                        typeof(LocalTargetInfo),
+                        typeof(ProjectileHitFlags),
+                        typeof(bool),
+                        typeof(Thing),
+                        typeof(ThingDef)
+                    });
+
+                var methodInfoLaunchInterceptor = AccessTools.Method(typeof(Verb_ShootWithMode_Patch), nameof(LaunchInterceptor));
+
+                foreach (var inst in instructions)
+                {
+                    if (inst.Calls(methodInfoLaunch))
+                    {
+                        yield return new CodeInstruction(OpCodes.Ldarg_0);
+                        yield return new CodeInstruction(OpCodes.Call, methodInfoLaunchInterceptor);
+                    }
+                    else
+                    {
+                        yield return inst;
+                    }
+                }
+            }
+
+            _ = Transpiler(null);
+            throw new NotImplementedException("stub");
+        }
+
+        public static void LaunchInterceptor(
+            Projectile projectile,
+            Thing launcher,
+            Vector3 origin,
+            LocalTargetInfo usedTarget,
+            LocalTargetInfo intendedTarget,
+            ProjectileHitFlags hitFlags,
+            bool preventFriendlyFire,
+            Thing equipment,
+            ThingDef targetCoverDef,
+            Verb_LaunchProjectile verb)
+        {
+            if (verb is Verb_ShootWithMode verbShootExt)
+            {
+                var projectileCount = verbShootExt.EquipmentCompVerbModeSource?.CurrentVerbMode.projectilesPerShot;
+
+                for (int i = 0; i < projectileCount; ++i)
+                {
+                    projectile.Launch(launcher, origin, usedTarget, intendedTarget, hitFlags, preventFriendlyFire, equipment, targetCoverDef);
+                }
+            }
+            else
+            {
+                projectile.Launch(launcher, origin, usedTarget, intendedTarget, hitFlags, preventFriendlyFire, equipment, targetCoverDef);
+                return;
+            }
+        }
+    }
+
+}

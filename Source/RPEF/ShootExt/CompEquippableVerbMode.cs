bf6a33dff654447718c26cae5ac343da13f4c24a
gguake <gguak2@gmail.com>
2026-04-28 18:32:35 +0900

    Update CompEquippableVerbMode.cs

--- Source/RPEF/ShootExt/CompEquippableVerbMode.cs
+++ Source/RPEF/ShootExt/CompEquippableVerbMode.cs
@@ -21,17 +21,16 @@ namespace RPEF
         public string uiIconPath;
 
         [Unsaved(false)]
-        private Texture2D uiIconCached;
-
+        private Texture2D _uiIconCached;
         public Texture2D UIIcon
         {
             get
             {
-                if (uiIconCached == null && !uiIconPath.NullOrEmpty())
+                if (_uiIconCached == null && !uiIconPath.NullOrEmpty())
                 {
-                    uiIconCached = ContentFinder<Texture2D>.Get(uiIconPath);
+                    _uiIconCached = ContentFinder<Texture2D>.Get(uiIconPath);
                 }
-                return uiIconCached;
+                return _uiIconCached;
             }
         }
 
@@ -74,6 +73,43 @@ namespace RPEF
     public class CompProperties_EquippableVerbMode : CompProperties
     {
         public List<VerbMode> modes;
+
+        /// <summary>
+        /// 무기가 3개 이상의 모드를 지원하는 경우 모드 변경 UI Gizmo의 Label
+        /// 모드가 2개인 경우 변경할 모드의 정보가 출력됨
+        /// </summary>
+        public string changeModeGizmoLabel;
+
+        /// <summary>
+        /// 무기가 3개 이상의 모드를 지원하는 경우 모드 변경 UI Gizmo의 Description
+        /// 모드가 2개인 경우 변경할 모드의 정보가 출력됨
+        /// </summary>
+        public string changeModeGizmoDescription;
+
+        /// <summary>
+        /// 무기가 3개 이상의 모드를 지원하는 경우 모드 변경 UI Gizmo의 아이콘 경로
+        /// 모드가 2개인 경우 변경할 모드의 정보가 출력됨
+        /// </summary>
+        [NoTranslate]
+        public string changeModeGizmoUIIconPath;
+
+        [Unsaved(false)]
+        private Texture2D _changeModeGizmoUIIconCached;
+        public Texture2D ChangeModeGizmoUIIcon
+        {
+            get
+            {
+                if (_changeModeGizmoUIIconCached == null && !changeModeGizmoUIIconPath.NullOrEmpty())
+                {
+                    _changeModeGizmoUIIconCached = ContentFinder<Texture2D>.Get(changeModeGizmoUIIconPath);
+                }
+                return _changeModeGizmoUIIconCached;
+            }
+        }
+
+        /// <summary>
+        /// 무기의 Label 변경 기능을 장착한 상태에서만 적용할지 여부
+        /// </summary>
         public bool transformLabelOnlyEquipped = true;
 
         public CompProperties_EquippableVerbMode()
@@ -86,6 +122,7 @@ namespace RPEF
     {
         public CompProperties_EquippableVerbMode Props => (CompProperties_EquippableVerbMode)props;
 
+        public int CurrentVerbModeIndex => _verbModeIndex;
         public VerbMode CurrentVerbMode => Props.modes[_verbModeIndex];
 
         private int _verbModeIndex;
@@ -112,44 +149,7 @@ namespace RPEF
                 yield break;
             }
 
-            var current = CurrentVerbMode;
-            yield return new Command_Action
-            {
-                defaultLabel = current.label,
-                defaultDesc = current.description,
-                icon = current.UIIcon,
-                action = () =>
-                {
-                    if (Props.modes.Count == 2)
-                    {
-                        ChangeMode((_verbModeIndex + 1) % 2);
-                    }
-                    else
-                    {
-                        var options = new List<FloatMenuOption>();
-                        for (int i = 0; i < Props.modes.Count; i++)
-                        {
-                            if (i == _verbModeIndex)
-                            {
-                                continue;
-                            }
-
-                            int targetIndex = i;
-                            var mode = Props.modes[i];
-                            options.Add(new FloatMenuOption(
-                                label: mode.changeGizmoLabel ?? mode.label,
-                                action: () => ChangeMode(targetIndex),
-                                iconTex: mode.UIIcon,
-                                iconColor: Color.white)); ;
-                        }
-
-                        if (options.Count > 0)
-                        {
-                            Find.WindowStack.Add(new FloatMenu(options));
-                        }
-                    }
-                },
-            };
+            yield return new Command_EquippableVerbMode(this);
         }
 
         public override string TransformLabel(string label)
@@ -169,7 +169,7 @@ namespace RPEF
         public override float GetStatFactor(StatDef stat)
             => CurrentVerbMode.GetStatFactor(stat);
 
-        private void ChangeMode(int modeIndex)
+        public void ChangeMode(int modeIndex)
         {
             _verbModeIndex = modeIndex;
 
@@ -184,6 +184,124 @@ namespace RPEF
             {
                 var waitJob = JobMaker.MakeJob(JobDefOf.Wait, ticks, true);
                 pawn.jobs.StartJob(waitJob, JobCondition.InterruptForced);
+            }
+        }
+    }
+
+    public class Command_EquippableVerbMode : Command
+    {
+        private CompEquippableVerbMode PrimaryComp => _comps[0];
+        private List<CompEquippableVerbMode> _comps;
+
+        public Command_EquippableVerbMode(CompEquippableVerbMode comp)
+        {
+            _comps = new List<CompEquippableVerbMode>() { comp };
+
+            groupable = true;
+            groupKey = comp.parent.def.defNameHash;
+
+            icon = PrimaryComp.Props.modes.Count > 2 ? PrimaryComp.Props.ChangeModeGizmoUIIcon : PrimaryComp.Props.modes[(PrimaryComp.CurrentVerbModeIndex + 1) % 2].UIIcon;
+        }
+
+        public override string Label
+        {
+            get
+            {
+                if (PrimaryComp.Props.modes.Count > 2)
+                {
+                    return PrimaryComp.Props.changeModeGizmoLabel;
+                }
+                else
+                {
+                    var mode = PrimaryComp.Props.modes[(PrimaryComp.CurrentVerbModeIndex + 1) % 2];
+                    return mode.changeGizmoLabel ?? mode.label;
+                }
+            }
+        }
+
+        public override string Desc
+        {
+            get
+            {
+                if (PrimaryComp.Props.modes.Count > 2)
+                {
+                    return PrimaryComp.Props.changeModeGizmoDescription;
+                }
+                else
+                {
+                    var mode = PrimaryComp.Props.modes[(PrimaryComp.CurrentVerbModeIndex + 1) % 2];
+                    return mode.changeGizmoDescription ?? mode.description;
+                }
+            }
+        }
+
+        public override bool GroupsWith(Gizmo other)
+        {
+            if (_comps.Count == 0)
+            {
+                return false;
+            }
+
+            if (base.GroupsWith(other) && other is Command_EquippableVerbMode otherCommand)
+            {
+                if (otherCommand._comps.Count > 0 && PrimaryComp.parent.def == otherCommand.PrimaryComp.parent.def)
+                {
+                    // 모드가 2개면 동일한 모드끼리만 묶고, 3개 이상이면 전부 하나로 표시한다.
+                    if (PrimaryComp.Props.modes.Count > 2 || PrimaryComp.CurrentVerbModeIndex == otherCommand.PrimaryComp.CurrentVerbModeIndex)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public override void MergeWith(Gizmo other)
+        {
+            if (other is Command_EquippableVerbMode otherCommand)
+            {
+                _comps.AddRange(otherCommand._comps);
+            }
+        }
+
+        public override void ProcessInput(Event ev)
+        {
+            base.ProcessInput(ev);
+
+            var verbModeIndex = PrimaryComp.CurrentVerbModeIndex;
+            if (PrimaryComp.Props.modes.Count == 2)
+            {
+                var nextModeIndex = (verbModeIndex + 1) % 2;
+                for (int i = 0; i < _comps.Count; ++i)
+                {
+                    _comps[i].ChangeMode(nextModeIndex);
+                }
+            }
+            else
+            {
+                var options = new List<FloatMenuOption>();
+                for (int i = 0; i < PrimaryComp.Props.modes.Count; i++)
+                {
+                    if (_comps.Count == 1 && verbModeIndex == i) { continue; }
+
+                    var mode = PrimaryComp.Props.modes[i];
+
+                    options.Add(new FloatMenuOption(
+                        label: mode.changeGizmoLabel ?? mode.label,
+                        action: () => {
+                            for (int j = 0; j < _comps.Count; ++j)
+                            {
+                                _comps[j].ChangeMode(i);
+                            }
+                        },
+                        mouseoverGuiAction: mode.changeGizmoDescription != null ? (rect) => TooltipHandler.TipRegion(rect, mode.changeGizmoDescription) : null));
+                }
+
+                if (options.Count > 0)
+                {
+                    Find.WindowStack.Add(new FloatMenu(options));
+                }
             }
         }
     }
